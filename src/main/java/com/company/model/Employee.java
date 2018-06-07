a241bb9e132fd148ee50424b5a4631a13a9adbcf
Sancho Sebastine <sancho.sebastine@wnco.com>
2018-06-07 05:57:13 -0500

    Adding Valiation to Employee object

--- src/main/java/com/company/model/Employee.java
+++ src/main/java/com/company/model/Employee.java
@@ -3,14 +3,15 @@ package com.company.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Employee {
-    @Required  private String id;
-    @Required private String name;
+    @NotNull  private String id;
+    @NotNull private String name;
     private String telephone;
     private String userName;
     private String title;
